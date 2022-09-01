56b04194a2218c4b787e8fd9e1458ddb107b5851
koju <kojubivek@gmail.com>
2022-09-01 14:30:13 +1000

    updating our directory page with directory component and catefory-item component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./categories.styles.scss";
+import "./components/category-item/category-item.component";
+import "./components/directory/directory.component";
+import Directory from "./components/directory/directory.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Directory />;
+};
 
 export default App;
